f7598f79a9e17ec5e0b7bae49e9eda4f0d8c5a65
microl44 <71311176+microl44@users.noreply.github.com>
2022-06-07 00:31:13 +0200

    reeeee

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include "includers/header.php";
 include "Database.php";
+
 $sql = "SELECT * FROM movie ORDER BY nr";
 
 if ($result = $conn -> query($sql))
@@ -10,14 +11,14 @@ if ($result = $conn -> query($sql))
   echo "<br>";
   echo "<table border='1' style='font-weight: bold;'>";
     echo "<tr style='font-weight: italic;'>";
-    echo "<td id='Hnr'>Nr</td>";
-    echo "<td id='Hname'>Name</td>";
-    echo "<td id='Hgenre_namn'>Genre</td>";
-    echo "<td id='Hgrade'>IMDB Grade</td>";
-    echo "<td id='Hjayornay'>Jay or Nay</td>";
-    echo "<td id='Hpicked_by'>Picked By</td>";
-    echo "<td id='Hparticipants'>Participants</td>";
-    echo "<td id='His_major'>Wheel Type</td>";
+    echo "<td class='TableName'>Nr</td>";
+    echo "<td class='TableName'>Name</td>";
+    echo "<td class='TableName'>Genre</td>";
+    echo "<td class='TableName'>IMDB Grade</td>";
+    echo "<td class='TableName'>Jay or Nay</td>";
+    echo "<td class='TableName'>Picked By</td>";
+    echo "<td class='TableName'>Participants</td>";
+    echo "<td class='TableName'>Wheel Type</td>";
     echo "</tr>";
 
   while ($row = mysqli_fetch_assoc($result)) 
@@ -29,20 +30,17 @@ if ($result = $conn -> query($sql))
         echo "<td id='imdbGrade'>" . $row['imdb_rating'] . "</td>";
         echo "<td id='jayornay'>" . $row['jayornay'] . "</td>";
         echo "<td id='picked_by'>" . $row['picked_by'] . "</td>";
-	echo "<td id='participants'>" . $row['participants'] . "</td>";
-	if($row['is_major'] == 1)
-	{
-		echo "<td id='is_major'>Major</td>";
-	}
-	else
-	{
-		echo "<td id='is_major'>Minor</td>";
-	}
+	      echo "<td id='participants'>" . $row['participants'] . "</td>";
+	      if($row['is_major'] == 1)
+	      {
+	      	echo "<td id='is_major'>Major</td>";
+	      }
+	      else
+	      {
+	      	echo "<td id='is_major'>Minor</td>";
+	      }
       echo "</tr>";
   }
   echo "</table>";
-}
-else{
-echo "hello";
 }
 ?>
