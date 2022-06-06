3060b852b9046c9c0ec8a9c84379c4165cd4689c
microl44 <71311176+microl44@users.noreply.github.com>
2022-06-06 23:48:34 +0200

    reeee

--- includers/header.php
+++ includers/header.php
@@ -4,5 +4,6 @@
     <link type="text/css" href="Shared/style.css" rel="stylesheet">
     <head>
         <div id ='nav-bar'>
+          print_r("hellooooo");
         </div>
     </head>
--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!DOCTYPE HTML>
-
-<html>
-    <link type="text/css" href="Shared/style.css" rel="stylesheet">
-    <head>
-        <div id ='nav-bar'>
-          print_r("hellooooo");
-        </div>
-    </head>
-
 <?php
 include "includers/header.php";
 include "Database.php";
