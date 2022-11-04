b2188f7aa7a3513ffa864cdae99b6d76edf28be2
Behrad Tahamtani <beviseta@outlook.com>
2022-11-04 19:26:07 +0100

    refactor function alittle

---
+++ function.php
@@ -0,0 +1,44 @@
+<?php
+function PrintMovieTable($result,$stmp){
+  if ($result)
+  {
+    echo "<input type='hidden' id='hejsan'> <input type='hidden'></button></input>" ;
+    
+    echo "<br>";
+    echo "<table class='Table'>";
+    echo "<tr class='TableRow'>";
+    echo "<td class='TableItem TableHeader'>Nr</td>";
+    echo "<td class='TableItem TableHeader'>Name</td>";
+    echo "<td class='TableItem TableHeader'>Genre</td>";
+    echo "<td class='TableItem TableHeader'>IMDB Grade</td>";
+    echo "<td class='TableItem TableHeader'>Jay or Nay</td>";
+    echo "<td class='TableItem TableHeader'>Picked By</td>";
+    echo "<td class='TableItem TableHeader'>Participants</td>";
+    echo "<td class='TableItem TableHeader'>Wheel Type</td>";
+    echo "</tr>";
+    
+    while ($row = $stmp->fetch()) 
+    {
+      echo "<tr class='TableRow'>";
+      echo "<td class='TableItem'>" . $row['id'] . "</td>";
+      echo "<td class='TableItem' id='movieName'>" . $row['name'] . "</td>";
+      echo "<td class='TableItem'>" . $row['genre_name'] . "</td>";
+      echo "<td class='TableItem'>" . $row['imdb_rating'] . "</td>";
+      echo "<td class='TableItem'>" . $row['jayornay'] . "</td>";
+      echo "<td class='TableItem'>" . $row['picked_by'] . "</td>";
+      echo "<td class='TableItem'>" . $row['participants'] . "</td>";
+      if($row['is_major'] == 1)
+      {
+        echo "<td id='is_major'>Big Wheel</td>";
+      }
+      else
+      {
+        echo "<td id='is_major'>Small Wheel</td>";
+      }
+      echo "</tr>";
+    }
+    echo "</table>";
+    
+  }
+}
+?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include "includers/header.php";
 include "Database.php";
+include "function.php";
 
 if(isset($_POST['addName'])){
   try
@@ -47,7 +48,7 @@ if(isset($_POST['searchValue']))
   $temp = "%" . $_POST['searchValue'] ."%";
   $stmp->bindvalue(":input",$temp);
   $result = $stmp->execute();
-  printtable($result,$stmp);
+  PrintMovieTable($result,$stmp);
 
 }
 else
@@ -55,7 +56,7 @@ else
   $sql = "SELECT * FROM movie ORDER BY id";
   $stmp = $conn->prepare($sql);
   $result = $stmp-> execute();
-  printtable($result,$stmp);
+  PrintMovieTable($result,$stmp);
 }
 // show table end
 
@@ -89,50 +90,6 @@ echo "<option value='0'> Small Wheel </option>";
 echo "<input type='submit' value='add movie'/>";
 echo "</form>";
 // form end
-
-
-function printtable($result,$stmp){
-  if ($result)
-  {
-    echo "<input type='hidden' id='hejsan'> <input type='hidden'></button></input>" ;
-    
-    echo "<br>";
-    echo "<table class='Table'>";
-    echo "<tr class='TableRow'>";
-    echo "<td class='TableItem TableHeader'>Nr</td>";
-    echo "<td class='TableItem TableHeader'>Name</td>";
-    echo "<td class='TableItem TableHeader'>Genre</td>";
-    echo "<td class='TableItem TableHeader'>IMDB Grade</td>";
-    echo "<td class='TableItem TableHeader'>Jay or Nay</td>";
-    echo "<td class='TableItem TableHeader'>Picked By</td>";
-    echo "<td class='TableItem TableHeader'>Participants</td>";
-    echo "<td class='TableItem TableHeader'>Wheel Type</td>";
-    echo "</tr>";
-    
-    while ($row = $stmp->fetch()) 
-    {
-      echo "<tr class='TableRow'>";
-      echo "<td class='TableItem'>" . $row['id'] . "</td>";
-      echo "<td class='TableItem' id='movieName'>" . $row['name'] . "</td>";
-      echo "<td class='TableItem'>" . $row['genre_name'] . "</td>";
-      echo "<td class='TableItem'>" . $row['imdb_rating'] . "</td>";
-      echo "<td class='TableItem'>" . $row['jayornay'] . "</td>";
-      echo "<td class='TableItem'>" . $row['picked_by'] . "</td>";
-      echo "<td class='TableItem'>" . $row['participants'] . "</td>";
-      if($row['is_major'] == 1)
-      {
-        echo "<td id='is_major'>Big Wheel</td>";
-      }
-      else
-      {
-        echo "<td id='is_major'>Small Wheel</td>";
-      }
-      echo "</tr>";
-    }
-    echo "</table>";
-    
-  }
-}
 
 include "includers/footer.php";
 ?>
