01ea8c6784e7191a248c9604aaa2fbfb76f90297
Behrad Tahamtani <beviseta@outlook.com>
2022-11-04 16:47:23 +0100

    new branch

--- Database.php
+++ Database.php
@@ -1,6 +1,7 @@
 <?php
 	$username = "root";
-	$password = "tinytiger997";
+	$password = "";
 
-	$conn = new PDO("mysql:dbname=jul;host=localhost;port=3307", $username, $password);
+	$conn = new PDO("mysql:dbname=jul;host=localhost;port=3306", $username, $password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
