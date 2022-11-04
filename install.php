46bcb0360a49cac2581791926aafb6dc3e460c48
Behrad Tahamtani <beviseta@outlook.com>
2022-11-04 17:09:45 +0100

    REEEEEE

---
+++ install.php
@@ -0,0 +1,15 @@
+<?php
+include "includers/header.php";
+include "Database.php";
+
+try{
+    $stmp = $conn->prepare(file_get_contents('Shared/DatabaseInstallScript/CREATETABLESINSERTDATA.sql'));
+    $stmp->execute();
+    foreach($stmp->fetchall() as $row){print_r($row);}
+    echo "<h1> we good boizzzzz</h1>";
+}
+catch(Exception $e){
+    echo "<h1> ooh fuck ooh shit ooh fuck </h1>";
+}
+
+?>
