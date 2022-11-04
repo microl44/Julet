e61a4a1046d273948cda442efca405e3e89912df
Behrad Tahamtani <beviseta@outlook.com>
2022-11-04 20:03:51 +0100

    fixed bug update some shit

--- index.php
+++ index.php
@@ -77,25 +77,25 @@ echo "<h3>Add new movie </h3>";
 echo "<form action='index.php' method='POST'>";
 
 echo "<label for='addName'> Name</label>";
-echo "<input type='string' name='addName' required/> ";
+echo "<input type='text' name='addName' required/> ";
 
 echo "<label for='addGenre'> Genre </label>";
-echo "<input type='string' name='addGenre' requred /> <br/>";
+echo "<input type='text' name='addGenre' requred /> <br/>";
 
 echo "<label for='addIMDB'> IMDB Grade </label>";
-echo "<input type='string' name='addIMDB' requred /> ";
+echo "<input type='text' name='addIMDB' requred /> ";
 
 echo "<label for='addjayornay'> Jay or Nay </label>";
-echo "<input type='string' name='addjayornay' requred /> <br/>";
+echo "<input type='text' name='addjayornay' requred /> <br/>";
 
 echo "<label for='addPickedBy'> Picked BY </label>";
-echo "<input type='string' name='addPickedBy' requred /> ";
+echo "<input type='text' name='addPickedBy' requred /> ";
 
 echo "<label for='addParticipants'> Participants </label>";
-echo "<input type='string' name='addParticipants' requred /> <br/>";
+echo "<input type='text' name='addParticipants' requred /> <br/>";
 
 echo "<label for='addIs_major'>WheelType </label>";
-echo "<select name='addIs_major'>";
+echo "<select name='addIs_major' type='number'>";
 echo "<option value='1'> Big Wheel </option>";
 echo "<option value='0'> Small Wheel </option>";
 
