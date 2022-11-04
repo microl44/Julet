9d3c92e4573070c55f991925d6e1e4f89a504eea
Behrad Tahamtani <beviseta@outlook.com>
2022-11-04 18:10:14 +0100

    added search function

--- index.php
+++ index.php
@@ -2,14 +2,33 @@
 include "includers/header.php";
 include "Database.php";
 
-$sql = "SELECT * FROM movie ORDER BY id";
-
-if ($result = $conn -> query($sql))
+if(isset($_POST['input']))
 {
-  echo "<input type='hidden' id='hejsan'> <input type='hidden'></button></input>" ;
+  $sql = "SELECT * FROM movie WHERE picked_by LIKE :input";
+  $stmp = $conn->prepare($sql);
+  $temp = "%" . $_POST['input'] ."%";
+  $stmp->bindvalue(":input",$temp);
+  $result = $stmp->execute();
+  printtable($result,$stmp);
 
-  echo "<br>";
-  echo "<table class='Table'>";
+}
+else
+{
+
+  $sql = "SELECT * FROM movie ORDER BY id";
+  $stmp = $conn->prepare($sql);
+  $result = $stmp-> execute();
+  printtable($result,$stmp);
+}
+  
+
+function printtable($result,$stmp){
+  if ($result)
+  {
+    echo "<input type='hidden' id='hejsan'> <input type='hidden'></button></input>" ;
+    
+    echo "<br>";
+    echo "<table class='Table'>";
     echo "<tr class='TableRow'>";
     echo "<td class='TableItem TableHeader'>Nr</td>";
     echo "<td class='TableItem TableHeader'>Name</td>";
@@ -20,37 +39,38 @@ if ($result = $conn -> query($sql))
     echo "<td class='TableItem TableHeader'>Participants</td>";
     echo "<td class='TableItem TableHeader'>Wheel Type</td>";
     echo "</tr>";
-
-  while ($row = $result->fetch()) 
-  {
+    
+    while ($row = $stmp->fetch()) 
+    {
       echo "<tr class='TableRow'>";
-        echo "<td class='TableItem'>" . $row['id'] . "</td>";
-        echo "<td class='TableItem' id='movieName'>" . $row['name'] . "</td>";
-        echo "<td class='TableItem'>" . $row['genre_name'] . "</td>";
-        echo "<td class='TableItem'>" . $row['imdb_rating'] . "</td>";
-        echo "<td class='TableItem'>" . $row['jayornay'] . "</td>";
-        echo "<td class='TableItem'>" . $row['picked_by'] . "</td>";
-	      echo "<td class='TableItem'>" . $row['participants'] . "</td>";
-	      if($row['is_major'] == 1)
-	      {
-	      	echo "<td id='is_major'>Big Wheel</td>";
-	      }
-	      else
-	      {
-	      	echo "<td id='is_major'>Small Wheel</td>";
-	      }
+      echo "<td class='TableItem'>" . $row['id'] . "</td>";
+      echo "<td class='TableItem' id='movieName'>" . $row['name'] . "</td>";
+      echo "<td class='TableItem'>" . $row['genre_name'] . "</td>";
+      echo "<td class='TableItem'>" . $row['imdb_rating'] . "</td>";
+      echo "<td class='TableItem'>" . $row['jayornay'] . "</td>";
+      echo "<td class='TableItem'>" . $row['picked_by'] . "</td>";
+      echo "<td class='TableItem'>" . $row['participants'] . "</td>";
+      if($row['is_major'] == 1)
+      {
+        echo "<td id='is_major'>Big Wheel</td>";
+      }
+      else
+      {
+        echo "<td id='is_major'>Small Wheel</td>";
+      }
       echo "</tr>";
+    }
+    echo "</table>";
+    
   }
-  echo "</table>";
-
 }
-
 
 // serach for winner of wheel
 echo "<form action='index.php' method='POST'>";
 echo " Search for winner: <input type='string' name='input'/>";
 echo "<input type='submit' value='Search'/>";
 echo "</form>";
+// form end
 
 include "includers/footer.php";
 ?>
