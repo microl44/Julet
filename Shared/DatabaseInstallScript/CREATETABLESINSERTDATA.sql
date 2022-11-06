25c7db752924fcac11625280f9afeb98df22a878
microl44 <71311176+microl44@users.noreply.github.com>
2022-11-06 22:21:33 +0100

    Added more test data

--- Shared/DatabaseInstallScript/CREATETABLESINSERTDATA.sql
+++ Shared/DatabaseInstallScript/CREATETABLESINSERTDATA.sql
@@ -1,4 +1,3 @@
-DROP DATABASE IF EXISTS Jul;
 CREATE DATABASE IF NOT EXISTS Jul;
 USE Jul;
 
@@ -32,14 +31,12 @@ FOREIGN KEY (picked_by) REFERENCES participant(name)
 ) Engine = InnoDB;
 
 CREATE TABLE participated(
-participant int,
-movie int,
-PRIMARY KEY (participant, movie),
-FOREIGN KEY (participant) REFERENCES participant(id),
-FOREIGN KEY (movie) REFERENCES movie(id)
+movieID int,
+participantID int,
+PRIMARY KEY (movieID, participantID),
+FOREIGN KEY (participantID) REFERENCES participant(id),
+FOREIGN KEY (movieID) REFERENCES movie(id)
 ) ENGINE = InnoDB;
-
-# --------PROCEDURES-------#
 
 # --------INSERTS----------#
 
@@ -53,7 +50,7 @@ VALUES ("Micke"), ("Gabbe"), ("Crippe"), ("Behrad"), ("Linus"), ("Momme"), ("Vic
 
 INSERT INTO movie(name, genre_name, imdb_rating, jayornay, picked_by, participants, is_major)
 VALUES("Beauty and the beast (live-action)", "Romance", 7.1, "Jay", "Gabbe", "Micke, Gabbe, Crippe", 1), 
-("Catch me if you can", "Biography", 8.1, "Jay", "Gabbe", "Micke, Crippe, Gabbe, Behrad", 1), ("1917", "War", 8.2, "Jay", "Micke", "Micke, Gabbe", 0), 
+("Catch me if you can", "Biography", 8.1, "Jay", "Gabbe", "Micke, Gabbe, Crippe, Behrad", 1), ("1917", "War", 8.2, "Jay", "Micke", "Micke, Gabbe", 0), 
 ("Rocky IV", "Sport", 6.8, "Mexican Standoff", "Momme", "Micke, Gabbe, Crippe, Momme", 1), ("Aliens", "Wildcard", 8.4, "Jay", "Linus", "Micke, Gabbe, Crippe, Linus", 1), 
 ("Spider-Man: Into the spider-verse", "Family", 8.4, "Jay", "Behrad", "Micke, Gabbe, Crippe, Behrad, Momme", 1), ("The Green Mile", "Crime", 8.6, "Jay", "Micke", "Micke, Gabbe, Crippe, Linus", 1), 
 ("Sinister", "Horror", 6.8, "Jay", "Linus", "Micke, Gabbe, Crippe, Linus", 1), ("Annihilation", "Science-Fiction", 6.8, "Nay", "Linus", "Micke, Gabbe, Crippe, Linus", 1), 
@@ -64,4 +61,11 @@ VALUES("Beauty and the beast (live-action)", "Romance", 7.1, "Jay", "Gabbe", "Mi
 ("Inglorious basterds", "War", 8.3, "Jay", "UNK", "Micke, Behrad, Crippe, Linus", 0), ("A bug's life", "Adventure", 7.2, "Jay", "Micke", "Micke, Crippe, Linus", 0), 
 ("Zootopia", "Crime", 8, "Jay", "Micke", "Micke, Crippe, Linus, Behrad, Momme", 0), ("Treasure planet", "Animation", 7.2, "Jay", "Gabbe", "Micke, Crippe, Gabbe, Linus", 1), 
 ("The Wolf of Wall Street", "Biography", 8.2, "Jay", "Crippe", "Micke, Crippe, Linus, Behrad", 0), ("The revenant", "Western", 8, "Mexican standoff", "Crippe", "Micke, Crippe", 0),
-("Alita: Battle Angel", "CUSTOM RULES", 7.3, "Jay", "Micke", "Micke, Crippe, Behrad", 0), ("Baby Driver","Crime", 7.6, "Jay" , "Micke", "Micke, Crippe, Linus", 0);
+("Alita: Battle Angel", "CUSTOM RULES", 7.3, "Jay", "Micke", "Micke, Crippe, Behrad", 0), ("Baby Driver","Crime", 7.6, "Jay" , "Micke", "Micke, Crippe, Linus", 0), 
+("Lilo & Stitch", "Fantasy", 7.3, "Jay", "Momme", "Micke, Crippe, Behrad, Momme", 0);
+
+INSERT INTO participated(movieID, participantID)
+VALUES (1,1), (1,2), (1,3), (2,1), (2,2), (2,3), (2,4), (3,1), (3,2), (4,1), (4,2), (4,3), (4,6), (5,1), (5,2), (5,3), (5,5), (6,1), (6,2), (6,3), (6,4), (6,5), (7,1), (7,2), (7,3), (7,5),
+(8,1), (8,2), (8,3), (8,5), (9,1), (9,2), (9,3), (9,5), (10,1), (10,5), (10,8), (11,1), (11,2), (11,3), (11,5), (12,1), (12,3), (12,4), (12,5), (12,6), (12,7), (13,1), (13,6), (14,1), (14,3), 
+(14,5), (15,1), (15,2), (15,3), (15,4), (15,6), (15,7), (16,1), (16,3), (16,4), (16,5), (16,6), (17,1), (17,4), (17,5), (18,1), (18,3), (18,4), (18,5), (19,1), (19,3), (19,5), (20,1), (20,3), 
+(20,4), (20,5), (20,6), (21,1), (21,2), (21,3), (21,5), (22,1), (22,3), (22,4), (22,5), (23,1), (23,3), (24,1), (24,3), (24,4), (25,1), (25,3), (25,5), (26,1), (26,3), (26,4), (26,6);
