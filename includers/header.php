820057863573ed4c53c9365fb8c584fa78774a15
microl44 <miro96@gmail.com>
2022-12-23 18:42:06 +0100

    accidentally created circular dependency

--- includers/header.php
+++ includers/header.php
@@ -3,7 +3,7 @@ if(!isset($_SESSION))
 { session_start();}
 
 require_once "loginFunctions.php";
-require_once "includers/header.php";
+require_once "includers/basic.php";
 ?>
 <link type="text/css" href="Shared/style.css" rel="stylesheet">
 <head>
