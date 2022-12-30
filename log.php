59e07fb99e15a82e665a9bc462377f9ac84110ae
microl44 <miro96@gmail.com>
2022-12-30 16:54:51 +0100

    :big_brother_emoji: :eyes_emoji:

--- log.php
+++ log.php
@@ -8,31 +8,27 @@ require_once "includers/basic.php";
 
 function addLog($activity = 'pageview')
 {
-	if(exists($_SESSION['consent'] && $_SESSION['consent'] == True))
+	if(exists($_SESSION['username']))
 	{
-		if(exists($_SESSION['username']))
-		{
-			$conn = GetConnection();
+		$conn = GetConnection();
 
-			$ip = getClientIP();
-			$page_url = parse_url($_SERVER['REQUEST_URI']);
-			$path = $page_url['path'];
-			$page = basename($path);
+		$ip = getClientIP();
+		$page_url = parse_url($_SERVER['REQUEST_URI']);
+		$path = $page_url['path'];
+		$page = basename($path);
 
-			$sql = "INSERT INTO activity_log (username, action, data) VALUES(:username, :action, :data)";
-			$stmt = $conn->prepare($sql);
+		$sql = "INSERT INTO activity_log (username, action, data) VALUES(:username, :action, :data)";
+		$stmt = $conn->prepare($sql);
 
-			$stmt->bindParam(':username', $username, PDO::PARAM_STR);
-			$stmt->bindParam(':action', $action, PDO::PARAM_STR);
-			$stmt->bindParam(':data', $data, PDO::PARAM_STR);
+		$stmt->bindParam(':username', $username, PDO::PARAM_STR);
+		$stmt->bindParam(':action', $action, PDO::PARAM_STR);
+		$stmt->bindParam(':data', $data, PDO::PARAM_STR);
 
-			$username = sanitizeInput($_SESSION['username']);
-			$action = $activity;
-			$data = json_encode(array('page_ulr' => $page, 'ip_address' => $ip));
-
-			$stmt->execute();
-		}	
-	}
+		$username = sanitizeInput($_SESSION['username']);
+		$action = $activity;
+		$data = json_encode(array('page_ulr' => $page, 'ip_address' => $ip));
+		$stmt->execute();
+	}	
 }
 
 
