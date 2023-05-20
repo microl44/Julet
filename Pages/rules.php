22f93fb14bdb51c50ffd96cfbaac8e0faceb2169
microl44 <71311176+microl44@users.noreply.github.com>
2023-05-20 22:07:26 +0200

    changed a 1 to a 2 in a text string. Pointless commit

--- Pages/rules.php
+++ Pages/rules.php
@@ -7,7 +7,7 @@
 	require_once "../includers/header.php";
 
 	$generalInfo = "The following is the rules of the Jul/Wheel/Christmas: A Genre-Jul is spun to determine what genre the Jul will be. Then each participant will have roughly 15 minutes to pick a movie within that genre and bring it to the wheel.";
-	$titles = array("Regular Wheel (1+)", "Reverse Wheel (2+)", "Dealers choice (3+)", "Multi-choice Wheel (1+)", "Reverse Multi-wheels of madness (1+)", "Tag-Team Wheel (3+)", "Custom Wheel");
+	$titles = array("Regular Wheel (2+)", "Reverse Wheel (2+)", "Dealers choice (3+)", "Multi-choice Wheel (1+)", "Reverse Multi-wheels of madness (1+)", "Tag-Team Wheel (3+)", "Custom Wheel");
 	$descriptions = array("This is a simple wheel. Each participant brings a single movie to the Jul, the Jul is spun and whatever movie it lands on is the winner.",
 						"Reverse wheel. Every participant brings a movie to the Jul, the Jul is spun and the winning result is removed from the Jul. The Jul is then re-spun until only 1 remains which is the winner.",
 						"Each user brings a movie to the wheel, but instead of spinning the movies, the users names are spun. The one it lands on gets to decide between the movies the other users brought.", "Each user brings more than one movie to the Jul. Can be combined with other rules to create Reverse-Multi-Wheels of madness.",
