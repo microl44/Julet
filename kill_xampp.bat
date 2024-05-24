1467af9cc3e70ee3d985d9edd706f185b90d3a95
microl44 <71311176+microl44@users.noreply.github.com>
2024-05-25 01:40:12 +0200

    Added kill script for xampp (crashes on exit by default)

--- kill_xampp.bat
+++ kill_xampp.bat
@@ -1,2 +1,10 @@
 tasklist /fi "ImageName eq xampp-control.exe" /fo csv 2>NUL | find /I "xampp-control.exe">NUL
-if "%ERRORLEVEL%"=="0" echo Program is running
+if "%ERRORLEVEL%"=="0" echo xampp-control.exe dead
+
+tasklist /fi "ImageName eq httpd.exe" /fo csv 2>NUL | find /I "httpd.exe">NUL
+if "%ERRORLEVEL%"=="0" echo httpd.exe dead
+
+tasklist /fi "ImageName eq mysqld.exe" /fo csv 2>NUL | find /I "mysqld.exe">NUL
+if "%ERRORLEVEL%"=="0" echo mysqld.exe dead
+
+pause
