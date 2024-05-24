d702ffafc08c9dfc0b9e7bc507a685c1ce9074a6
microl44 <71311176+microl44@users.noreply.github.com>
2024-05-25 01:49:57 +0200

    Debug fix. Should work semi-properly now.

--- kill_xampp.bat
+++ kill_xampp.bat
@@ -1,10 +1,11 @@
 tasklist /fi "ImageName eq xampp-control.exe" /fo csv 2>NUL | find /I "xampp-control.exe">NUL
-if "%ERRORLEVEL%"=="0" echo xampp-control.exe dead
+if "%ERRORLEVEL%"=="0" taskkill /F /IM xampp-control.exe
 
 tasklist /fi "ImageName eq httpd.exe" /fo csv 2>NUL | find /I "httpd.exe">NUL
-if "%ERRORLEVEL%"=="0" echo httpd.exe dead
+if "%ERRORLEVEL%"=="0" taskkill /F /IM httpd.exe
 
 tasklist /fi "ImageName eq mysqld.exe" /fo csv 2>NUL | find /I "mysqld.exe">NUL
-if "%ERRORLEVEL%"=="0" echo mysqld.exe dead
+if "%ERRORLEVEL%"=="0" taskkill /F /IM mysqld.exe
 
+task
 pause
