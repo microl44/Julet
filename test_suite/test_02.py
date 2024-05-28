24edf6a7416072dbab87985e0069c150e62cf713
microl44 <71311176+microl44@users.noreply.github.com>
2024-05-28 21:40:54 +0200

    Stashed work...
    working on test suite & shared lib

--- test_suite/test_02.py
+++ test_suite/test_02.py
@@ -3,4 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import EdgeOptions
 from selenium import webdriver
+
 from time import sleep
+
+import shared.basic
