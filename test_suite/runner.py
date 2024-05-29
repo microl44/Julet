a6849f9504d9b193a1b2b464b8fa9627141c8c38
microl44 <71311176+microl44@users.noreply.github.com>
2024-05-29 15:41:18 +0200

    Added creation of test_res folder if not already exists.

--- test_suite/runner.py
+++ test_suite/runner.py
@@ -5,6 +5,9 @@ import ctypes
 
 cases=['01','02', '03']
 
+if not os.path.exists("C:/xampp/htdocs/Julet/test_suite"):
+    os.makedirs("C:/xampp/htdocs/Julet/test_suite")
+
 for case in cases:
 	cmd = "python C:/xampp/htdocs/Julet/test_suite/test_" + case + ".py"
 	os.system("start /wait cmd /c {}".format(cmd))
