19813d7a67eca86e8829ad6c1e3a04e40c45482a
microl44 <miro96@gmail.com>
2024-07-24 00:01:44 +0200

    Added linux support for fetch script.

--- fetch.py
+++ fetch.py
@@ -7,15 +7,22 @@ class GitPipe():
     git_location = None
     git_branch = None 
 
-    def __init__(self, path='C:/xampp/htdocs/Julet', account=[None, None]):
-        with open('c:/xampp/htdocs/Julet/.git/HEAD', 'r') as file:
+    if sys.platform == "win32":
+        platform = "windows"
+    else:
+        platform = "linux"
+
+    JUL_ROOT = os.getenv('JUL_ROOT')
+
+    def __init__(self, path=JUL_ROOT, account=[None, None]):
+        with open(self.JUL_ROOT + '/.git/HEAD', 'r') as file:
             line = file.read().split('/')[-1].replace('\n', '')
-        
+
         if line == "main":
             print("\nCurrent branch is MAIN. No issue can be fetched...\n")
             exit()
-        
-        res = subprocess.run(['powershell', 'c:/xampp/htdocs/Julet/fetch.ps1', line], capture_output=True, shell=True, text=True)
+
+        res = subprocess.run(['powershell', self.JUL_ROOT + '/fetch.ps1', line], capture_output=True, shell=True, text=True)
         print("\n")
         print(res.stdout)
 
