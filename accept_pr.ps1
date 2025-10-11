7deb5a825fc78118e656302d92f3224103f0465c
microl44 <this@isexample.com>
2025-10-11 13:53:18 +0200

    dirk dirk dirk dirk dirk

--- accept_pr.ps1
+++ accept_pr.ps1
@@ -1,7 +1,7 @@
-$token=<PAT TOKEN>
-$owner=<GITHUB USERNAME>
-$repo=<REPO NAME>
-$mergeMethod="rebase"
+$token=$env:CPR_TOKEN
+$owner=$env:CPR_OWNER
+$repo=$env:CPR_REPO
+$mergeMethod=rebase
 
 $response = Invoke-RestMethod `
     -Uri "https://api.github.com/repos/$owner/$repo/pulls?state=open&sort=updated&direction=desc&per_page=1" `
